d54799eee8a8ee8f8bfc77889e35d0bb60166870
Mustafa <43754271+Djama613@users.noreply.github.com>
2018-10-26 15:20:33 -0400

    Second attempt
    For some reason Git thinks this is a whole new file when it is not.

---
+++ reset_password.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    /*
+     * --------------------------------------------------------------------------
+     * Reset Password Language Lines
+     * --------------------------------------------------------------------------
+     *
+     * The following language lines are used on the reset password form
+     *
+     */
+    
+    'title' => 'Reset Password',
+    'return' => "Return Home",
+    'email' => 'E-Mail Address',
+    'password' => 'Password',
+    'confirm_password' => 'Confirm Password',
+    'submit' => 'Reset Password',
+    'password_copy' => 'Create a password that contains at least one character from each of the following categories: lower-case characters (a-z), upper-case characters (A-Z), digits (0-9), and non-alphanumeric symbols (%, $, !, etc.).',
+    'auth_logo' => '/images/logo_tc_colour.png'
+];
